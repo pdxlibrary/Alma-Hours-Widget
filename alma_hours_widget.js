04eb49bab192c3748ad6d6cda9d912318d7af0d5
Mike Flakus <mflakus@pdx.edu>
2015-12-02 11:22:48 -0800

    added work-around to account for Alma API not returning correct data when multiple dates are queried at the same time added handling for 24 hours added handling for multiple open periods in the same day

--- alma_hours_widget.js
+++ alma_hours_widget.js
@@ -8,8 +8,6 @@ jQuery(document).ready(function(){
 	// load all widgets
 	jQuery(".alma_hours_widget").each(function(){
 		
-		console.log("start...");
-		
 		// extract html5 parameters for widgets
 		var input_library = jQuery(this).attr("data-library");
 		var input_title = jQuery(this).attr("data-title");
@@ -19,19 +17,28 @@ jQuery(document).ready(function(){
 		var input_time_format = jQuery(this).attr("data-time-format");
 		var widget = $(this);
 		
-		console.log(" | " + input_library + " | " + input_start_date + " | " + input_end_date + " | " + input_date_format + " | " + input_time_format);
+		// console.log(" | " + input_library + " | " + input_start_date + " | " + input_end_date + " | " + input_date_format + " | " + input_time_format);
 		
 		// load hours
 		$.getJSON( script_path, { library: input_library, from: input_start_date, to: input_end_date, date_format: input_date_format, time_format: input_time_format } )
 		.done(function( json ) {
-			console.log("got hours...");
 			var widget_days = [];
 			$.each(json,function(date,day) {
-				console.dir(day);
 				if(day.closed)
 					widget_days.push( "<li class='alma_hours_row'><span class='alma_hours_row_date'>"+day.date+"</span><span class='alma_hours_row_closed'>Closed</span></li>");
+				else if(day.open24hours)
+					widget_days.push( "<li class='alma_hours_row'><span class='alma_hours_row_date'>"+day.date+"</span><span class='alma_hours_row_open24hours'>Open 24 Hours</span></li>");
 				else
-					widget_days.push( "<li class='alma_hours_row'><span class='alma_hours_row_date'>"+day.date+"</span><span class='alma_hours_row_open'>" + day.open + "</span>-<span class='alma_hours_row_close'>" + day.close + "</span></li>" );
+				{
+					var hours_html = "";
+					$.each(day.hours,function(index,hours) {
+						if(hours_html == "")
+							hours_html = "<span class='alma_hours_row_open'>" + hours.open + "</span>-<span class='alma_hours_row_close'>" + hours.close + "</span>";
+						else
+							hours_html += ", <span class='alma_hours_row_open'>" + hours.open + "</span>-<span class='alma_hours_row_close'>" + hours.close + "</span>";
+					});
+					widget_days.push( "<li class='alma_hours_row'><span class='alma_hours_row_date'>"+day.date+"</span>"+hours_html+"</li>" );
+				}
 			});
 			$(widget).html($( "<ul/>",{"class": "alma_hours_list",html: widget_days.join("")})).prepend("<div class='alma_hours_widget_title'>"+input_title+"</div>").show();
 		})
--- alma_hours_widget.php
+++ alma_hours_widget.php
@@ -5,7 +5,7 @@
 // set your Alma Hours API Key
 define("ALMA_HOURS_API_KEY","XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX");
 
-// set the Caching Frequency - Daily, Hourly or None (default: Daily)
+// set the Caching Frequency - Daily, Hourly or None (recommended default: Daily)
 define("CACHE_FREQUENCY","Daily");
 
 // if this file is not on the same host as the widget JavaScript file, cross-site scripting (XSS) access needs to be allowed
@@ -20,6 +20,9 @@ else
 	header('Access-Control-Allow-Origin: none');
 
 $hours = array();
+
+$ch = curl_init();
+curl_setopt($ch,CURLOPT_RETURNTRANSFER, true);
 
 // REQUIRED PARAMETERS
 if(isset($_GET['library']))
@@ -43,153 +46,151 @@ else
 	$time_format = "g:ia";
 
 if(isset($_GET['from']))
-	$from = urlencode($_GET['from']);
+	$from = date("Y-m-d",strtotime("+1 day",strtotime($_GET['from'])));
 else
-	$from = "";
+	$from = date("Y-m-d",strtotime("+1 day"));
 
 if(isset($_GET['to']))
-	$to = urlencode(date("Y-m-d",strtotime("+1 day",strtotime($_GET['to']))));	// need to increase by one day, because Alma hours API is not inclusive of range
+	$to = date("Y-m-d",strtotime("+1 day",strtotime($_GET['to'])));	// need to increase by one day, because Alma hours API is not inclusive of range
 else
-	$to = "";
+	$to = date("Y-m-d",strtotime("+7 days"));
 
 
-
-
-// BUILD REST REQUEST URL
-$url = "https://api-ap.hosted.exlibrisgroup.com/almaws/v1/conf/libraries/".$library."/open-hours?apikey=".ALMA_HOURS_API_KEY;
-if(strcmp($from,''))
-	$url .= "&from=".$from;
-if(strcmp($to,''))
-	$url .= "&to=".$to;
-
-if(isset($_GET['debug']))
-	print("URL: $url<br>\n");
-
-
-$xml_result = false;
-
-if(strcmp(CACHE_FREQUENCY,"None"))
+// each date must be queried by itself to work-around an issue with the API returning invalid data for multi-day queries
+for($date = date("Y-m-d",strtotime($from)); strtotime($date) <= strtotime($to); $date = date("Y-m-d",strtotime("+1 day",strtotime($date))))
 {
-	// check cache for hours
-	if(file_exists("cache/$library-$from-$to.xml"))
+	$xml_result = false;
+	
+	// BUILD REST REQUEST URL
+	$url = "https://api-ap.hosted.exlibrisgroup.com/almaws/v1/conf/libraries/".$library."/open-hours?apikey=".ALMA_HOURS_API_KEY;
+	if(strcmp($from,''))
+		$url .= "&from=".$date;
+	if(strcmp($to,''))
+		$url .= "&to=".$date;
+
+	if(isset($_GET['debug']))
+		print("URL: $url<br>\n");
+	
+	if(strcmp(CACHE_FREQUENCY,"None"))
 	{
-		// check last modified datestamp
-		$cache_expired = false;
-		switch(CACHE_FREQUENCY)
+		// check cache for hours
+		if(file_exists("cache/".$library."_".$date.".xml"))
 		{
-			case 'Hourly': if(filemtime("cache/$library-$from-$to.xml") < strtotime(date("Y-m-d H:00:00",strtotime("now")))) $cache_expired = true;
-			default: if(filemtime("cache/$library-$from-$to.xml") < strtotime(date("Y-m-d 00:00:00",strtotime("now")))) $cache_expired = true;
+			// check last modified datestamp
+			$cache_expired = false;
+			switch(CACHE_FREQUENCY)
+			{
+				case 'Hourly': if(filemtime("cache/".$library."_".$date.".xml") < strtotime(date("Y-m-d H:00:00",strtotime("now")))) $cache_expired = true;
+				default: if(filemtime("cache/".$library."_".$date.".xml") < strtotime(date("Y-m-d 00:00:00",strtotime("now")))) $cache_expired = true;
+			}
+			// $cache_expired = true;
+			if(!$cache_expired)
+			{
+				$xml_result = simplexml_load_file("cache/".$library."_".$date.".xml");
+				if(isset($_GET['debug'])) print("loaded data from cache file: cache/".$library."_".$date.".xml<br>\n");
+			}
 		}
-		if(!$cache_expired)
-			$xml_result = simplexml_load_file("cache/$library-$from-$to.xml");
 	}
-}
-
-// if no cache data available, query the Alma API
-if(!$xml_result)
-{
-	// use curl to make the API request
-	$ch = curl_init();
-	curl_setopt($ch,CURLOPT_URL, $url);
-	curl_setopt($ch,CURLOPT_RETURNTRANSFER, true);
-	$result = curl_exec($ch);
-	curl_close($ch);
 	
-	// parse the response xml
-	$xml_result = simplexml_load_string($result);
-	
-	// save result to cache
-	if(strcmp(CACHE_FREQUENCY,"None") && is_writable("cache/$library-$from-$to.xml"))
+	// if no cache data available, query the Alma API
+	if(!$xml_result)
 	{
-		file_put_contents("cache/$library-$from-$to.xml",$result);
+		// use curl to make the API request
+		curl_setopt($ch,CURLOPT_URL, $url);
+		$result = curl_exec($ch);
+		
+		if(isset($_GET['debug']))
+		{
+			print("xml result from API<br>\n");
+			print("<pre>".htmlspecialchars($result)."</pre>");
+		}
+				
+		// save result to cache
+		if(strcmp(CACHE_FREQUENCY,"None") && is_writable("cache/".$library."_".$date.".xml"))
+		{
+			file_put_contents("cache/".$library."_".$date.".xml",$result);
+		}
+		
+		$xml_result = simplexml_load_string($result);
+	}
+	
+	// PARSE RESULTS
+	if(!isset($xml_result->day))
+	{
+		// work around for missing date info for full day closures
+		$hours_obj = new stdClass();
+		$actual_date = date("Y-m-d",strtotime("-1 day",strtotime($date)));
+		$hours_obj->date = gmdate($date_format,strtotime($actual_date));
+		$hours_obj->closed = true;
+		$open_hours[$actual_date] = $hours_obj;
+	}
+	else
+	{
+		foreach($xml_result->day as $day)
+		{
+			if(strtotime($day->date) > 0)
+			{
+				$hours_obj = new stdClass();
+				
+				$formatted_date = gmdate($date_format,strtotime($day->date));
+				$hours_obj->date = $formatted_date;
+				
+				// foreach open-close block for this date, collect the hours
+				foreach($day->hours->hour as $hours)
+				{
+					if(strtotime($day->date.$hours->from) > 0 && strtotime($day->date.$hours->to) > 0 )
+					{
+						// Midnight Fix
+						if(!strcmp(gmdate("g:ia",strtotime($day->date.$hours->from)),"12:00am"))
+							$formatted_open_time = "Midnight";
+						else
+							$formatted_open_time = gmdate($time_format,strtotime($day->date.$hours->from));
+						if(!strcmp(gmdate("g:ia",strtotime($day->date.$hours->to)),"11:59pm"))
+							$formatted_close_time = "Midnight";
+						else
+							$formatted_close_time = gmdate($time_format,strtotime($day->date.$hours->to));
+						
+						if(!strcmp($formatted_open_time,'Midnight') && !strcmp($formatted_close_time,'Midnight'))
+						{
+							// special case - open 24 hours
+							$hours_obj->open24hours = true;
+						}
+						else
+						{
+							// standard hours
+							$hours_obj->hours[] = array("open"=>$formatted_open_time,"close"=>$formatted_close_time);
+						}
+					}
+					else
+					{
+						// Full Day Closure
+						$hours_obj->closed = true;
+					}
+				}
+				$open_hours[gmdate("Y-m-d",strtotime($day->date))] = $hours_obj;
+			}
+		}
+	}
+	
+	if(isset($_GET['debug']))
+	{
+		print("<pre>\n");
+		print_r($xml_result);
+		print("</pre>\n");
 	}
 }
 
 // CREATE JSON OUTPUT
-if(strcmp($from,''))
-	$expected_start_date = gmdate("Y-m-d",strtotime("yesterday",strtotime($from)));
-else
-	$expected_start_date = gmdate("Y-m-d",strtotime("yesterday",strtotime("now")));
-if(isset($_GET['to']))
-	$expected_end_date = gmdate("Y-m-d",strtotime($_GET['to']));
-else
-	$expected_end_date = gmdate("Y-m-d",strtotime("+6 days"));
-$expected_current_date = $expected_start_date;
-foreach($xml_result->day as $day)
-{
-	if(strtotime($day->date) > 0)
-	{
-		// work around for missing date info for full day closures
-		$breakout_count = 0;
-		while(strcmp(gmdate("Y-m-d",strtotime($day->date)),$expected_current_date))
-		{
-			$formatted_date = gmdate($date_format,strtotime($expected_current_date));
-			$hours[$expected_current_date] = array("date"=>$formatted_date,"closed"=>true);
-			$expected_current_date = gmdate("Y-m-d",strtotime("tomorrow",strtotime($expected_current_date)));
-			
-			if(strtotime($expected_current_date) > strtotime($expected_end_date))
-				break;
-			
-			$breakout_count++;
-			if($breakout_count > 30)
-			{
-				break;
-			}
-		}
-		
-		$formatted_date = gmdate($date_format,strtotime($day->date));
-		
-		if(strtotime($day->date.$day->hours->hour->from) > 0 && strtotime($day->date.$day->hours->hour->to) > 0 )
-		{
-			// Midnight Fix
-			if(!strcmp(gmdate("g:ia",strtotime($day->date.$day->hours->hour->from)),"11:59pm"))
-				$formatted_open_time = "Midnight";
-			else
-				$formatted_open_time = gmdate($time_format,strtotime($day->date.$day->hours->hour->from));
-			if(!strcmp(gmdate("g:ia",strtotime($day->date.$day->hours->hour->to)),"11:59pm"))
-				$formatted_close_time = "Midnight";
-			else
-				$formatted_close_time = gmdate($time_format,strtotime($day->date.$day->hours->hour->to));
-			
-			$hours[gmdate("Y-m-d",strtotime($day->date))] = array("date"=>$formatted_date,"open"=>$formatted_open_time,"close"=>$formatted_close_time);
-		}
-		else
-		{
-			// Full Day Closure
-			$hours[gmdate("Y-m-d",strtotime($day->date))] = array("date"=>$formatted_date,"closed"=>true);
-		}
-		$expected_current_date = gmdate("Y-m-d",strtotime("tomorrow",strtotime($expected_current_date)));
-	}
-}
-
-// workaround to include missing full day closures at the end of the result set
-$breakout_count = 0;
-while(strtotime($expected_current_date) <= strtotime($expected_end_date))
-{
-	$formatted_date = gmdate($date_format,strtotime($expected_current_date));
-	$hours[$expected_current_date] = array("date"=>$formatted_date,"closed"=>true);
-	$expected_current_date = gmdate("Y-m-d",strtotime("tomorrow",strtotime($expected_current_date)));
-	
-	$breakout_count++;
-	if($breakout_count > 30)
-	{
-		print("ERROR: breakout!!<br>\n");
-		break;
-	}
-}
-
-
-print(json_encode($hours));
+print(json_encode($open_hours));
 
 if(isset($_GET['debug']))
 {
 	print("<pre>\n");
-	print_r($hours);
-	print_r($xml_result);
+	print("calculated hours: \n");
+	print_r($open_hours);
 	print("</pre>\n");
 }
 
-
-
+curl_close($ch);
 
 ?>
